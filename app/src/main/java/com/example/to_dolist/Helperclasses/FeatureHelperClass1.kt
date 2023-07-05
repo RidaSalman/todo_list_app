809e8eec8151b0c193c08ece6fee40cfa3415370
Rida Salman <ridasalman458@gmail.com>
2023-07-06 03:54:48 +0500

    filing

---
+++ app/src/main/java/com/example/to_dolist/Helperclasses/FeatureHelperClass1.kt
@@ -0,0 +1,11 @@
+package com.example.to_dolist.Helperclasses
+
+import android.graphics.drawable.Drawable
+
+
+data class FeaturedHelperClass1(
+    val image1: Drawable?,
+    val title1: String,
+    val description1: String,
+    val color1:Int,
+)
