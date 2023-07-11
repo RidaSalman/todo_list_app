905bb0c4da02f199bebb0e22f9cf6e3c03ad5c75
Rida Salman <ridasalman458@gmail.com>
2023-07-11 16:48:23 +0500

    set bottom navigation

--- app/src/main/java/com/example/to_dolist/Adapters/FeaturedAdapter.kt
+++ app/src/main/java/com/example/to_dolist/Adapters/FeaturedAdapter.kt
@@ -1,78 +1,126 @@
 package com.example.to_dolist.Adapters
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.to_dolist.CreateTaskFragment
 import com.example.to_dolist.Helperclasses.FeaturedHelperClass
+import com.example.to_dolist.MainActivity
 import com.example.to_dolist.R
+import com.example.to_dolist.UserInfo
 
-class FeaturedAdapter(private val dataList: ArrayList<FeaturedHelperClass>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class FeaturedAdapter(private val dataList: ArrayList<FeaturedHelperClass>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     // Define the view types
-    private val VIEW_TYPE_1 = 1
-    private val VIEW_TYPE_2 = 2
-    private val VIEW_TYPE_3 = 3
-
+    companion object {
+        private const val VIEW_TYPE_FIRST = 0
+        private const val VIEW_TYPE_SECOND = 1
+        private const val VIEW_TYPE_THIRD = 2
+    }
     // Implement the necessary methods: getItemCount, getItemViewType, and onCreateViewHolder
 
     override fun getItemCount(): Int {
-        return dataList.size
+        return dataList.size + 2 // Add 2 for the first static view, second static view, and the third view's title
     }
 
     override fun getItemViewType(position: Int): Int {
-        val data = dataList[position]
         return when (position) {
-            0 -> VIEW_TYPE_1
-            1 -> VIEW_TYPE_2
-            else -> VIEW_TYPE_3
+            0 -> VIEW_TYPE_FIRST
+            1 -> VIEW_TYPE_SECOND
+            2 -> VIEW_TYPE_THIRD
+            else -> VIEW_TYPE_THIRD
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
-            VIEW_TYPE_1 -> {
-                val view = inflater.inflate(R.layout.row_item_home_header, parent, false)
-                Type1ViewHolder(view)
+            VIEW_TYPE_FIRST -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.row_item_home_header, parent, false)
+                FirstViewHolder(view)
             }
-            VIEW_TYPE_2 -> {
-                val view = inflater.inflate(R.layout.row_item_section_title, parent, false)
-                Type2ViewHolder(view)
+
+            VIEW_TYPE_SECOND -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.row_item_section_title, parent, false)
+                SecondViewHolder(view)
             }
-            else -> {
-                val view = inflater.inflate(R.layout.row_item_task, parent, false)
-                Type3ViewHolder(view)
+
+            VIEW_TYPE_THIRD -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.row_item_task, parent, false)
+                ThirdViewHolder(view)
             }
+
+            else -> throw IllegalArgumentException("Invalid view type")
         }
     }
 
 
-    inner class Type1ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class FirstViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textView1: TextView = itemView.findViewById(R.id.textViewOnGoingTaskCount)
+        private val textView2: TextView = itemView.findViewById(R.id.textViewCompleteTaskCount)
+        private val textView3: TextView = itemView.findViewById(R.id.textViewCancelTaskCount)
+        private val cardView: CardView = itemView.findViewById(R.id.cardViewAddNew)
+
+
+        fun bindFirstView() {
+            cardView.setOnClickListener {
+                val context = itemView.context
+                openCreateTaskScreen(context)
+            }
+
+        }
+
+    }
+    inner class SecondViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindSecondView() {
+        }
     }
 
-    inner class Type2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    }
+    inner class ThirdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textView: TextView = itemView.findViewById(R.id.textViewTitle1)
+        private val cardView: CardView = itemView.findViewById(R.id.mainBackground)
 
-    inner class Type3ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var taskBackground : CardView = itemView.findViewById(R.id.mainBackground)
+        fun bindThirdView(data: FeaturedHelperClass) {
+            textView.text = data.title
+            cardView.setCardBackgroundColor(data.color)
+        }
     }
 
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val data = dataList[position]
-        when (holder) {
-            is Type1ViewHolder -> {
-                // Bind data and handle clicks for view type 1
+        when (holder.itemViewType) {
+            VIEW_TYPE_FIRST -> {
+                val firstViewHolder = holder as FirstViewHolder
+                firstViewHolder.bindFirstView()
             }
-            is Type2ViewHolder -> {
-                // Bind data and handle clicks for view type 2
-            }
-            is Type3ViewHolder -> {
 
-               holder.taskBackground.setCardBackgroundColor(data.color)
+            VIEW_TYPE_SECOND -> {
+                val secondViewHolder = holder as SecondViewHolder
+                secondViewHolder.bindSecondView()
+            }
+
+            VIEW_TYPE_THIRD -> {
+                val thirdViewHolder = holder as ThirdViewHolder
+                val dataIndex =
+                    position - 2 // Adjust the index to account for the first two static views
+                if (dataIndex >= 0 && dataIndex < dataList.size) {
+                    thirdViewHolder.bindThirdView(dataList[dataIndex])
+                }
             }
         }
+    }
+
+    private fun openCreateTaskScreen(context: Context) {
+        val intent = Intent(context, UserInfo::class.java)
+        context.startActivity(intent)
     }
 }
--- app/src/main/java/com/example/to_dolist/Helperclasses/FeaturedHelperClass.kt
+++ app/src/main/java/com/example/to_dolist/Helperclasses/FeaturedHelperClass.kt
@@ -7,3 +7,10 @@ data class FeaturedHelperClass(
     val title: String,
     val color: Int = 0,
 )
+
+data class FeaturedHelperClass1(
+    val image1: Drawable?,
+    val title1: String,
+    val description1: String,
+    val color1:Int,
+)
