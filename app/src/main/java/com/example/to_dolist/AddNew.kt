59f72db8df431f4eaa6d8328f90ab5b78b04a5a8
Rida Salman <ridasalman458@gmail.com>
2023-07-18 17:09:23 +0500

    Resolve issue with date picker in Add New Task activity

--- app/src/main/java/com/example/to_dolist/AddNew.kt
+++ app/src/main/java/com/example/to_dolist/AddNew.kt
@@ -1,35 +1,56 @@
 package com.example.to_dolist
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.DatePicker
 import android.widget.PopupMenu
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import java.util.Calendar
 
 class AddNew : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new)
 
-        val descTextInputLayout = findViewById<TextInputLayout>(R.id.desc)
+        val items = listOf("Work","Shopping", "Fun", "Category")
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete)
+        val adapter = ArrayAdapter(this, R.layout.list_item,items)
+        autoComplete.setAdapter(adapter)
+        autoComplete.onItemClickListener=AdapterView.OnItemClickListener{
+            adapterView, view, i, l ->
 
-        descTextInputLayout.setEndIconOnClickListener {
-            showDropdownMenu(it)
-        }
-    }
-
-    private fun showDropdownMenu(view: View) {
-        val popupMenu = PopupMenu(this, view)
-        popupMenu.menuInflater.inflate(R.menu.category_menu, popupMenu.menu)
-
-        popupMenu.setOnMenuItemClickListener { item: MenuItem ->
-            val selectedCategory = item.title.toString()
-            Toast.makeText(this, "Selected Category: $selectedCategory", Toast.LENGTH_SHORT).show()
-            true
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this,"Item:$itemSelected",Toast.LENGTH_SHORT).show()
         }
 
-        popupMenu.show()
+        val editText: TextInputEditText = findViewById(R.id.time)
+        val calendar = Calendar.getInstance()
+
+        val initialYear = calendar.get(Calendar.YEAR)
+        val initialMonth = calendar.get(Calendar.MONTH)
+        val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
+        editText.setText("$initialYear-${initialMonth + 1}-$initialDay")
+
+        editText.setOnClickListener {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { view: DatePicker?, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+                editText.setText("$selectedYear-${selectedMonth + 1}-$selectedDay")
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
     }
 }
